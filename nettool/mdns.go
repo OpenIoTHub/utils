0d944699af920663dc21810c86ce409a28917471
Farry <newfarry@126.com>
2019-10-28 15:19:18 +0800

    update

--- nettool/mdns.go
+++ nettool/mdns.go
@@ -11,10 +11,8 @@ import (
 )
 
 var MDNSServiceBaseInfo = map[string]string{
-	"name": "OpenIoTHub服务",
-	//web,native,none
-	"ui-support":           "none",
-	"ui-first":             "none",
+	"name":                 "OpenIoTHub服务",
+	"model":                "com.iotserv.services.web",
 	"author":               "Farry",
 	"email":                "newfarry@126.com",
 	"home-page":            "https://github.com/OpenIoTHub",
